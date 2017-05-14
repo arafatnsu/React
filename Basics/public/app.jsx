169ca1c598f11d30cbb96ff0aa21ba2cfaee33bd
arafatnsu <arafatnsu@yahoo.com>
2017-05-14 23:06:43 +0300

    basic

---
+++ Basics/public/app.jsx
@@ -0,0 +1,24 @@
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React'
+    };
+  },
+  render: function () {
+    var name = this.props.name;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>This is from a component!</p>
+      </div>
+    );
+  }
+});
+
+var firstName = "Yasir";
+
+ReactDOM.render(
+<Greeter name={firstName}/>,
+document.getElementById('app')
+);
