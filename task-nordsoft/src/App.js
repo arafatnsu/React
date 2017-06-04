9f2299eb74eb0400876a569f62e11f4a1bb061fd
arafatnsu <arafatnsu@yahoo.com>
2017-06-04 07:09:28 +0300

    add new user

--- task-nordsoft/src/App.js
+++ task-nordsoft/src/App.js
@@ -8,8 +8,16 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      user: {}
+      users: {},
+      user: {
+        name: '',
+        email: '',
+        phone: ''
+      },
     }
+    this.addNewUser = this.addNewUser.bind(this)
+    this.updateField = this.updateField.bind(this)
+    this.validateInput = this.validateInput.bind(this)
   }
 
   getUsers() {
@@ -17,9 +25,46 @@ class App extends Component {
       url:'http://localhost:3000/emailList.json',
       type: 'GET',
       success:function(data) {
-        this.setState({user: data.emailData})
+        this.setState({users: data.emailData})
       }.bind(this),
     })
+  }
+
+  updateField(e) {
+    let user = this.state.user
+    user[e.target.name] = e.target.value
+    this.setState({user: user})
+  }
+
+  validateInput() {
+    const user = this.state.user
+    if(user.name === '' || user.email === '' || user.phone === '') {
+      alert('Required field is missing');
+      return;
+    }
+    else {
+      return true;
+    }
+  }
+
+  addNewUser() {
+    this.validateInput()
+    if(this.validateInput() === true) {
+      let oldUsers = this.state.users
+      let user = this.state.user
+      oldUsers.push(user)
+      this.setState({users: oldUsers})
+      user = {
+        name: '',
+        email: '',
+        phone: '',
+      }
+      alert('New User was added')
+      this.setState({user: user})
+    }
+    else {
+      return
+    }
   }
 
   componentDidMount() {
@@ -27,11 +72,15 @@ class App extends Component {
   }
 
   render() {
-    const users = this.state.user
+    const users = this.state.users
     return (
       <div className="App">
         <div>
-          <InputData />
+          <InputData
+            addUser={this.addNewUser}
+            user={this.state.user}
+            updateValue={this.updateField}
+          />
         </div>
         <div>
           <div className="row user-header-info">
--- task-nordsoft/src/Components/InputData.js
+++ task-nordsoft/src/Components/InputData.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const InputData = () => (
+const InputData = ({updateValue, user, addUser}) => (
   <div className="input-field-header">
     <div className="col-xs-3 input-field">
-      <input className="box-sizing" type="text" placeholder="Full name" />
+      <input value={user.name} name="name" onChange={e => updateValue(e)} className="box-sizing" type="text" placeholder="Full name" required/>
     </div>
     <div className="col-xs-4 input-field">
-      <input className="box-sizing-email" type="email" placeholder="E-mail address" />
+      <input value={user.email} name="email" onChange={e => updateValue(e)} className="box-sizing-email" type="email" placeholder="E-mail address" required/>
     </div>
     <div className="col-xs-3 input-field">
-      <input className="box-sizing" type="phone" placeholder="Phone number" />
+      <input value={user.phone} name="phone" onChange={e =>updateValue(e)} className="box-sizing" type="number" placeholder="Phone number" required/>
     </div>
     <div className="col-xs-2 pull-right">
-      <button type="button" className="add-user-button pull-right box-sizing">Add new</button>
+      <button type="button" onClick={addUser} className="add-user-button pull-right box-sizing">Add new</button>
     </div>
   </div>
 )
